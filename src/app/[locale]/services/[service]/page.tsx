6be6341597f27c18651bb9752ce52c8ac9a4acf1
Ehab97 <ehabreda04@gmail.com>
2025-08-09 15:48:52 +0300

    fix build issue

--- src/app/[locale]/services/[service]/page.tsx
+++ src/app/[locale]/services/[service]/page.tsx
@@ -1,10 +1,15 @@
 import { ServicePage } from "@/components/services/ServicePage";
 import React from "react";
 
-const page = ({ params }: { params: { service: string; locale: "ar" | "en" } }) => {
-  const { service, locale } = params;
+interface PageParams {
+  params: Promise<{
+    service: string;
+    locale: "ar" | "en";
+  }>;
+}
+
+export default async function Page({ params }: PageParams) {
+  const { service, locale } = await params;
 
   return <ServicePage currentService={service} language={locale} />;
-};
-
-export default page;
+}
